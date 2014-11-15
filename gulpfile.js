da638b8ead3f6802d897d5b8b62babe2e6ec0192
oMicrone <omicrone@gmail.com>
2014-11-15 02:26:55 +0100

    Gulp now inlines critical CSS into HTML and reloads, when html or less is updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,33 @@ var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
 var critical = require('critical');
+var file = require('gulp-file');
+var inlinesource = require('gulp-inline-source');
+var copy = require('gulp-copy');
+var debug = require('gulp-debug');
 
 var reload = browserSync.reload;
-var runLess = ['less', 'my-critical'];
 
 gulp.task('default', function() {
 
 	gulp.watch(['*.html', 'css/**/*.css', 'js/**/*.js', 'img/**/*.*'], {cwd: 'Deploy'}, reload);
-  	gulp.watch(['css/**/*.less'], {cwd: 'Code'}, runLess);
+  gulp.watch(['css/**/*.less'], {cwd: 'Code'}, ['less', 'my-critical']);
+  gulp.watch(['*.html'], {cwd: 'Code'}, ['my-critical']);
 
-  	gulp.start('less', 'serve', 'my-critical');
+  gulp.start('serve', 'less', 'my-critical');
 });
 
 // watch files for changes and reload
 gulp.task('serve', function() {
   browserSync({
     server: {
-      baseDir: 'Deploy'
+      baseDir: './Deploy'
     }
   });
 });
 
 gulp.task('less', function () {
-	gulp.src('Code/css/main.less')
+	return gulp.src('Code/css/main.less')
   	.pipe(sourcemaps.init())
   	.pipe(less())
     .pipe(autoprefixer({
@@ -37,17 +41,37 @@ gulp.task('less', function () {
       cascade: false
     }))
  	 	.pipe(minifyCSS())
-  	.pipe(sourcemaps.write('maps', {sourceRoot: '/Deploy'}))
-  	.pipe(gulp.dest('Deploy/css'))  
+  	.pipe(sourcemaps.write('maps', {sourceRoot: 'Deploy'}))
+  	.pipe(gulp.dest('Deploy/css'));
 });
 
-gulp.task('my-critical', function () {
+gulp.task('my-critical', ['less'], function () {
+
   critical.generate({
-    base: 'Deploy/',
+    base: 'Deploy',
     src: 'index.html',
+    css: 'Deploy/css/main.css',
     dest: 'css/critical.css',
     minify: true,
     width: 1024,
     height: 760
+  }, function (err, o) {
+    gulp.start('inlinesource');
   });
+
+});
+
+gulp.task('copy-html', function () {
+  return gulp.src('Code/*.html')
+             .pipe(gulp.dest('Deploy'));
+});
+
+gulp.task('inlinesource', ['copy-html'], function () {
+    return gulp.src('Deploy/*.html')
+        .pipe(inlinesource({rootpath: 'Deploy'}))
+        .pipe(gulp.dest('Deploy'));
+});
+
+gulp.task('reload', function () {
+  reload();
 });
